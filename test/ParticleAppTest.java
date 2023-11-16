5e5f2aff235dc2f6ea752fd8e9c13f0138df190d
jaymoreno7 <JMoreno3@mlock.com>
2023-11-15 21:03:03 -0600

    Added ParticlePanel tests and Particle tests.

--- test/ParticleAppTest.java
+++ test/ParticleAppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,16 +15,6 @@ public class ParticleAppTest {
     private final int SIZE = 1024;
     private final int THREAD_COUNT = 128;
 
-    /*
-    Set the close operation to dispose the window rather than exit the application.
-     This is **needed** for testing because of the WindowConstants.EXIT_ON_CLOSE in the app method.
-     */
-    private void setupJFrameForTest() {
-        particleApp.SetupJFrame();
-        particleApp.jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    }
-
-
     @BeforeEach
     public void setUp() {
         particleApp = new ParticleApp(SIZE, THREAD_COUNT);
@@ -31,8 +22,10 @@ public class ParticleAppTest {
 
     @Test
     public void testParticleAppInitializationWithValidArguments() {
-        assertNotNull(particleApp.scheduler, "Scheduler should not be null after initialization");
-        assertNotNull(particleApp.panel, "Panel should not be null after initialization");
+        assertNotNull(particleApp.scheduler,
+                "Scheduler should not be null after initialization");
+        assertNotNull(particleApp.panel,
+                "Panel should not be null after initialization");
     }
 
     @Test
@@ -40,12 +33,24 @@ public class ParticleAppTest {
         assertThrows(IllegalArgumentException.class, () -> new ParticleApp(-1, THREAD_COUNT));
     }
 
+    /**
+     * Set the close operation to dispose the window rather than exit the application.
+     * This is **needed** for testing because of the WindowConstants.EXIT_ON_CLOSE in the app method.
+     */
+    private void setupJFrameForTest() {
+        particleApp.SetupJFrame();
+        particleApp.jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+    }
+
     @Test
     public void testSetupJFrame() throws InterruptedException, InvocationTargetException {
         SwingUtilities.invokeAndWait(this::setupJFrameForTest);
-        assertTrue(particleApp.jFrame.isVisible(), "JFrame should be visible after setup");
-        assertEquals(SIZE, particleApp.jFrame.getSize().width, "JFrame should have the correct size");
-        assertEquals(SIZE, particleApp.jFrame.getSize().height, "JFrame should have the correct size");
+        assertTrue(particleApp.jFrame.isVisible(),
+                "JFrame should be visible after setup");
+        assertEquals(SIZE, particleApp.jFrame.getSize().width,
+                "JFrame should have the correct size");
+        assertEquals(SIZE, particleApp.jFrame.getSize().height,
+                "JFrame should have the correct size");
     }
 
     @Test
@@ -59,7 +64,51 @@ public class ParticleAppTest {
                 break;
             }
         }
-        assertTrue(hasParticlePanel, "ParticlePanel should be added to JFrame");
+        assertTrue(hasParticlePanel,
+                "ParticlePanel should be added to JFrame");
+    }
+
+
+    @Test
+    public void testParticlePanelRepaintCalled() throws InterruptedException, InvocationTargetException {
+        TestParticlePanel testPanel = new TestParticlePanel(SIZE);
+        particleApp.panel = testPanel;
+
+        // Simulate the app starting which should trigger repaint
+        particleApp.Start();
+        SwingUtilities.invokeAndWait(this::setupJFrameForTest);
+
+        // Verify that repaint was called
+        assertTrue(testPanel.repaintCalled,
+                "repaint should be called on ParticlePanel");
+    }
+
+    @Test
+    public void testParticleMoveWithParameters() {
+        Particle particle = new Particle(50, 50);
+        particle.move(5, 10);
+        assertTrue(particle.x >= 45 && particle.x <= 55,
+                "Particle should move within the horizontal range");
+        assertTrue(particle.y >= 40 && particle.y <= 60,
+                "Particle should move within the vertical range");
+    }
+
+    @Test
+    public void testParticleDefaultMove() {
+        Particle particle = new Particle(50, 50);
+        particle.move();
+        assertTrue(particle.x >= 45 && particle.x <= 55,
+                "Particle should move within the default horizontal range");
+        assertTrue(particle.y >= 40 && particle.y <= 60,
+                "Particle should move within the default vertical range");
+    }
+
+    @Test
+    public void testParticleDraw() {
+        Particle particle = new Particle(50, 50);
+        Graphics image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB).createGraphics();
+        assertDoesNotThrow(() -> particle.draw(image),
+                "Draw method should execute without throwing exceptions");
     }
 
     @AfterEach
@@ -68,5 +117,22 @@ public class ParticleAppTest {
             particleApp.jFrame.dispose();
         }
         particleApp.Stop();
+    }
+
+    /**
+     * Test class for Particle Panel in lieu of setting up a mock environment.
+     */
+    private static class TestParticlePanel extends ParticlePanel {
+        boolean repaintCalled = false;
+
+        public TestParticlePanel(int size) {
+            super(size);
+        }
+
+        @Override
+        public void repaint() {
+            repaintCalled = true;
+            super.repaint();
+        }
     }
 }
